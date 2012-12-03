06b03b9d9f509d676f461700f9d000f3406c63ac
Keith Burnell <keith@dotnetdevdude.com>
2012-12-02 20:55:15 -0600

    Removed database set initializer

--- CodeFirstMagicUnicornAndBeyond/End/Demo1/Program.cs
+++ CodeFirstMagicUnicornAndBeyond/End/Demo1/Program.cs
@@ -9,7 +9,7 @@ namespace Demo1 {
 
     class Program {
         static void Main(string[] args) {
-            Database.SetInitializer(new DataContextInitializer());
+            //Database.SetInitializer(new DataContextInitializer());
             Demo1DataContext context = new Demo1DataContext();
             IList <Manufacturer> manufacturers = context.Manufacturers.ToList();
             Console.WriteLine(string.Format("Ferrari is {0} miles from Lamborghini.", Math.Round(manufacturers[0].Location.Distance(manufacturers[1].Location).Value / 1609.344, 2)));
