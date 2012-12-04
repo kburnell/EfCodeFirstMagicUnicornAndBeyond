0c7e35c0cbd1db2c4aaa0fea9c29bc59cbe628e2
Keith Burnell <kb@dotnetdevdude.com>
2012-12-04 13:53:56 -0600

    Added some cool testing stuff

---
+++ CodeFirstMagicUnicornAndBeyond/Begin/Demo1.Tests.Integration/DataContext/TestDataContextInitializer.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+using Demo1.DataContext;
+
+namespace Demo1.Tests.Integration.DataContext
+{
+    public class TestDataContextInitializer : DropCreateDatabaseIfModelChanges<Demo1DataContext>
+    {
+    }
+}
---
+++ CodeFirstMagicUnicornAndBeyond/Begin/Demo1.Tests.Integration/IntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Demo1.DataContext;
+using Demo1.Domain;
+using Demo1.Tests.Integration.DataContext;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo1.Tests.Integration {
+
+    [TestClass]
+    public class IntegrationTests {
+
+        [TestMethod]
+        public void All_Manufacturers_ShouldReturn_4_Manufacturers() {
+            //Arrange
+            Database.SetInitializer(new TestDataContextInitializer());
+            Demo1DataContext _db = new Demo1DataContext();
+            //Act
+            IList<Manufacturer> result = _db.Manufacturers.ToList();
+            //Assert
+            result.Count().Should().Be(4);
+        }
+    }
+}
