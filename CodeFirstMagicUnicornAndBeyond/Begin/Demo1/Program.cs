084c19ba2110f8d738959f4e855e70707e1aa173
Keith Burnell <kburnell@skylinetechnologies.com>
2013-11-01 08:36:52 -0500

    Couple of Updates

--- CodeFirstMagicUnicornAndBeyond/Begin/Demo1/Program.cs
+++ CodeFirstMagicUnicornAndBeyond/Begin/Demo1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,9 +10,15 @@ namespace Demo1 {
 
     class Program {
         static void Main(string[] args) {
+
             Database.SetInitializer(new DataContextInitializer());
             Demo1DataContext context = new Demo1DataContext();
+
+            //THIS IS DEMO-WARE --- DO NOT DO THIS IN PRODUCTION -- Please Have a Proper Data Access Layer!!!!
             IList <Manufacturer> manufacturers = context.Manufacturers.ToList();
+
+            Console.Write("Tada!");
+            Console.ReadKey();
         }
     }
 }
